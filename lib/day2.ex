cdaf44714d6da43a9b3fdcb485b4f957b1fe3463
Moritz Schmale <narrow.m@gmail.com>
2021-12-02 11:47:59 +0100

    formatting

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,17 +1,19 @@
 defmodule Day2 do
   def part_1 do
-    %{x: x, y: y} = input()
-    |> Enum.reduce(%{x: 0, y: 0}, fn
-      {:forward, x}, acc -> Map.update!(acc, :x, &(&1 + x))
-      {:up, y}, acc -> Map.update!(acc, :y, &(&1 - y))
-      {:down, y}, acc -> Map.update!(acc, :y, &(&1 + y))
-    end)
+    %{x: x, y: y} =
+      input()
+      |> Enum.reduce(%{x: 0, y: 0}, fn
+        {:forward, x}, acc -> Map.update!(acc, :x, &(&1 + x))
+        {:up, y}, acc -> Map.update!(acc, :y, &(&1 - y))
+        {:down, y}, acc -> Map.update!(acc, :y, &(&1 + y))
+      end)
 
     IO.puts(x * y)
   end
 
   def part_2 do
     acc = %{depth: 0, horizontal: 0, aim: 0}
+
     %{depth: d, horizontal: h} =
       input()
       |> Enum.reduce(acc, fn
@@ -20,8 +22,11 @@ defmodule Day2 do
           |> Map.update!(:depth, &(&1 + x * acc.aim))
           |> Map.update!(:horizontal, &(&1 + x))
 
-          {:up, x}, acc -> Map.update!(acc, :aim, &(&1 - x))
-          {:down, x}, acc -> Map.update!(acc, :aim, &(&1 + x))
+        {:up, x}, acc ->
+          Map.update!(acc, :aim, &(&1 - x))
+
+        {:down, x}, acc ->
+          Map.update!(acc, :aim, &(&1 + x))
       end)
 
     IO.puts(d * h)
